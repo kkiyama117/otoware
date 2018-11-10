5482aaf8bb61776d55800769627dd4b774e4f544
kkiyama117 <k.kiyama117@gmail.com>
2018-11-11 00:03:44 +0900

    Update print
    出力を分かりやすくした

--- src/convert_wav_and_array.py
+++ src/convert_wav_and_array.py
@@ -19,19 +19,20 @@ def wav_to_ndarray(path: Path):
     with path.open("rb") as f, wave.open(f) as wf:
         wav_data = get_wave_info(path=path)
         length = wav_data['frames']
+        print(get_wave_info(path=path))
         return wf.readframes(length)
 
 
-def ndarray_to_wav(data: bytes, channel: int, fs: int, result_path: Path):
+def ndarray_to_wav(data: bytes, channel: int, fs: int, path: Path):
     """波形データをWAVEファイルへ出力"""
-    with result_path.open("wb") as f, wave.open(f, "w") as wf:
+    with path.open("wb") as f, wave.open(f, "w") as wf:
         wf.setnchannels(channel)
         # 16bit // 2 で サンプルサイズは2(らしい)
         wf.setsampwidth(2)
         wf.setframerate(fs)
         wf.writeframes(data)
         print("data completely saved")
-    print(get_wave_info(path=result_path))
+    print(get_wave_info(path=path))
 
 
 def normalization(array) -> list:
