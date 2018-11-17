72413ee81f7e39d85c5c487f6d01be1bdae242cf
kkiyama117 <k.kiyama117@gmail.com>
2018-11-18 01:23:19 +0900

    Update

--- src/convert_wav_and_array.py
+++ src/convert_wav_and_array.py
@@ -6,87 +6,92 @@ from pathlib import Path
 from numpy.ma import frombuffer
 
 
-def wav_to_ndarray(path: Path):
-    with path.open("rb") as f, wave.open(f) as wf:
-        wav_data = get_wave_info(path=path)
-        length = wav_data['frames']
-        print(get_wave_info(path=path))
-        return wf.readframes(length)
+class WavAndArray:
+    def __init__(self, origin_path: Path, result_path: Path = None):
+        self._path: Path = origin_path
+        self._result_path = result_path
+        # 元のWavファイルから取得した音楽データ情報
+        self._wav_info = self.get_wave_info()
+        # ndarray化した音楽データ
+        self._data = self.wav_to_ndarray()
+        print(self._wav_info)
 
+    # byte列を(本来のwavのデータの情報を元に再生)
+    def play_file(self):
+        channel = self._wav_info['channel']
+        width = self._wav_info['width']
+        frame_rate = self._wav_info['frame_rate']
+        # numpyのndarrayをデバイスのデフォルトの再生機器に送信
+        self.ndarray_to_device(self._data, channel, width, frame_rate)
 
-def get_wave_info(path: Path):
-    """WAVEファイルの情報を取得"""
-    with path.open("rb") as f, wave.open(f) as wf:
-        return {'channel': wf.getnchannels(), 'width': wf.getsampwidth(),
-                'frame_rate': wf.getframerate(), 'frames': wf.getnframes(),
-                'params': wf.getparams(),
-                "long": float(wf.getnframes() / wf.getframerate())
-                }
+    # 初期化関数群
 
+    def get_wave_info(self):
+        """WAVEファイルの情報を取得"""
+        with self._path.open("rb") as f, wave.open(f) as wf:
+            return {'channel': wf.getnchannels(), 'width': wf.getsampwidth(),
+                    'frame_rate': wf.getframerate(), 'frames': wf.getnframes(),
+                    'params': wf.getparams(),
+                    "long": float(wf.getnframes() / wf.getframerate())
+                    }
 
-def normalization(array) -> list:
-    # エフェクトをかけやすいようにバイナリデータを[-1, +1]に正規化
-    # wav -> numpy ndArray
-    # int16 の絶対値は 32767
-    return frombuffer(array, dtype="int16") / 32768.0
+    def wav_to_ndarray(self):
+        with self._path.open("rb") as f, wave.open(f) as wf:
+            length = self._wav_info['frames']
+            return wf.readframes(length)
 
+    # 変換用関数群
+    def normalization(self, array) -> list:
+        # エフェクトをかけやすいようにバイナリデータを[-1, +1]に正規化
+        # ndArray -> ndArray
+        # int16 の絶対値は 32767
+        return frombuffer(array, dtype="int16") / 32768.0
 
-def de_normalization(array) -> bytes:
-    # 正規化前のバイナリデータに戻す(32768倍)
-    new_data = [int(x * 32767.0) for x in array]
-    return struct.pack("h" * len(new_data), *new_data)
+    def de_normalization(self, array) -> bytes:
+        # 正規化前のバイナリデータに戻す(32768倍)
+        new_data = [int(x * 32767.0) for x in array]
+        return struct.pack("h" * len(new_data), *new_data)
 
+    # 再生用関数
+    def ndarray_to_device(self, data: bytes, channel: int, width, rate):
+        p = pyaudio.PyAudio()
+        # Streamを生成(3)
+        stream = p.open(format=p.get_format_from_width(width),
+                        channels=channel,
+                        rate=rate,
+                        output=True)
+        size = len(data)
+        pos = 0  # byte count
+        while pos < size:
+            # frame_size
+            frame_size = 1024
+            o = data[pos:pos + frame_size]
+            stream.write(o)
+            pos += frame_size
+        # time.sleep(float(size) / 2 / rate)
+        stream.close()
+        p.terminate()
 
-def ndarray_to_device(data: bytes, channel: int, width, rate):
-    p = pyaudio.PyAudio()
-    # Streamを生成(3)
-    stream = p.open(format=p.get_format_from_width(width),
-                    channels=channel,
-                    rate=rate,
-                    output=True)
-    size = len(data)
-    pos = 0  # byte count
-    while pos < size:
-        # frame_size
-        frame_size = 1024
-        o = data[pos:pos + frame_size]
-        stream.write(o)
-        pos += frame_size
-    # time.sleep(float(size) / 2 / rate)
-    stream.close()
-    p.terminate()
-
-
-# byte列を(本来のwavのデータの情報を元に再生)
-def play_file(new_data, origin_path):
-    wav_info = get_wave_info(origin_path)
-    channel = wav_info['channel']
-    width = wav_info['width']
-    frame_rate = wav_info['frame_rate']
-    # numpyのndarrayをデバイスのデフォルトの再生機器に送信
-    ndarray_to_device(new_data, channel, width, frame_rate)
-
-
-"""保存用関数群"""
-
-
+# """保存用関数群"""
+#
+#
 # array をwav に変換して保存
-def save_file(bit_array, origin_path: Path, result_path: Path):
-    # 音声を保存
-    wav_info = get_wave_info(origin_path)
-    frame_rate = wav_info['frame_rate']
-    channel = wav_info['channel']
-    ndarray_to_wav(bit_array, channel=channel, rate=frame_rate,
-                   path=result_path)
-
-
-def ndarray_to_wav(data: bytes, channel: int, rate: int, path: Path):
-    """波形データをWAVEファイルへ出力"""
-    with path.open("wb") as f, wave.open(f, "w") as wf:
-        wf.setnchannels(channel)
-        # 16bit // 2 で サンプルサイズは2(らしい)
-        wf.setsampwidth(2)
-        wf.setframerate(rate)
-        wf.writeframes(data)
-        print("data completely saved")
-    print(get_wave_info(path=path))
+# def save_file(bit_array, origin_path: Path, result_path: Path):
+#     音声を保存
+#     wav_info = get_wave_info(origin_path)
+#     frame_rate = wav_info['frame_rate']
+#     channel = wav_info['channel']
+#     ndarray_to_wav(bit_array, channel=channel, rate=frame_rate,
+#                    path=result_path)
+#
+#
+# def ndarray_to_wav(data: bytes, channel: int, rate: int, path: Path):
+#     """波形データをWAVEファイルへ出力"""
+#     with path.open("wb") as f, wave.open(f, "w") as wf:
+#         wf.setnchannels(channel)
+#         16bit // 2 で サンプルサイズは2(らしい)
+# wf.setsampwidth(2)
+# wf.setframerate(rate)
+# wf.writeframes(data)
+# print("data completely saved")
+# print(get_wave_info(path=path))
--- src/distortion.py
+++ src/distortion.py
@@ -3,15 +3,6 @@ import numpy as np
 from src import convert_wav_and_array as cwa
 
 
-# deprecated
-# 今回は保存しないので使わない
-def create_distortion_file(origin_path: Path, result_path: Path, gain):
-    # distortion nd_array
-    new_data = distortion_array(origin_path, gain)
-    # 音声を保存
-    cwa.save_file(new_data, origin_path, result_path)
-
-
 # 音割れファイルの再生
 def play_distortion_file(origin_path: Path, gain):
     # distortion nd_array
@@ -21,12 +12,19 @@ def play_distortion_file(origin_path: Path, gain):
 
 
 # origin_path の　wavに対して, distortion した　arrayを返す
+# リアルタイム処理の時は多分これでは駄目
 def distortion_array(origin_path, gain=5):
     # 音声をロード→正規化→エフェクト→逆正規化
     return cwa.de_normalization(
         distortion(cwa.normalization(cwa.wav_to_ndarray(origin_path)), gain))
 
-# byte列に対して distortion の処理をして送る
+
+def realtime_distortion_array(origin_path, gain=5):
+    data = cwa.wav_to_ndarray(origin_path)
+    data_array = cwa.normalization(data)
+
+
+# byteもしくはbyte列に対して distortion の処理をして送る
 def distortion(data, gain, level=0.7, clip=True):
     """gain乗の値をもちいてdistortion
     data: numpy.ndarray
@@ -37,10 +35,19 @@ def distortion(data, gain, level=0.7, clip=True):
     # https://qiita.com/stringamp/items/4b6e344ddf878f5099c7#122-%E5%AE%9F%E8%A3%85
     # 単純にgain倍増幅する時は以下
     # for n in range(length):
-    # newdata = data * gain
+    # new_data = data * gain
     new_data = np.sign(data) * (1 - np.exp(-1 * gain * np.abs(data)))
     if clip:
         new_data = np.clip(new_data, -1.0, 1.0)
     # 音量を調整
     new_data *= level
     return new_data
+
+
+# deprecated
+# 今回は保存しないので使わない
+def create_distortion_file(origin_path: Path, result_path: Path, gain):
+    # distortion nd_array
+    new_data = distortion_array(origin_path, gain)
+    # 音声を保存
+    cwa.save_file(new_data, origin_path, result_path)
